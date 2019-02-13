1687b7559c4b332c76744832dedd6cd434acc782
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-02-13 02:03:02 -0800

    Make Ajax request to randomuser.me

--- assets/js/generate-users.js
+++ assets/js/generate-users.js
@@ -1,11 +1,28 @@
+const requestUsers = (gender) => {
+  const ajax = new XMLHttpRequest();
+
+  ajax.onload = () => {
+    if (ajax.status >= 200 && ajax.status < 300) {
+      console.log(JSON.parse(ajax.response));
+    } else {
+      console.log('The request failed!');
+    }
+  };
+
+
+  ajax.open('GET', 'https://randomuser.me/api/');
+  ajax.send();
+};
+
 const generateUsers = (event) => {
   if (event.target.id === 'maleBtn') {
-    //
+    requestUsers('male');
   } else if (event.target.id === 'femaleBtn') {
-    //
+    requestUsers('female');
   }
 };
 
+// Add Event Listeners
 const buttons = document.getElementsByClassName('button');
 
 for (let i = 0; i < buttons.length; i += 1) {
