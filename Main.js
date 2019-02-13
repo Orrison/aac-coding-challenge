850bc2c396afefa702c08b9b86819f8b03af4b62
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-02-12 23:21:17 -0800

    Setup routing for CSS and JS

--- Main.js
+++ Main.js
@@ -5,17 +5,37 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (error, pgResp) => {
-    if (error) {
-      res.writeHead(404);
-      res.write('Contents you are looking are Not Found');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(pgResp);
-    }
+  if (req.url.indexOf('.css') !== -1) {
+    const { url } = req;
+    const fileName = url.split('/').pop().split('.')[0];
+    fs.readFile(`${__dirname}/assets/css/${fileName}.css`, (err, data) => {
+      if (err) console.log(err);
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url.indexOf('.js') !== -1) {
+    const { url } = req;
+    const fileName = url.split('/').pop().split('.')[0];
+    fs.readFile(`${__dirname}/assets/js/${fileName}.js`, (err, data) => {
+      if (err) console.log(err);
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile('index.html', (error, pgResp) => {
+      if (error) {
+        res.writeHead(404);
+        res.write('Contents you are looking are Not Found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(pgResp);
+      }
 
-    res.end();
-  });
+      res.end();
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
