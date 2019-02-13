028aee74a921d85e96506de99d80c11609c8a320
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-02-13 13:32:51 -0800

    Create helper function to split results in 3s

--- assets/js/generate-users.js
+++ assets/js/generate-users.js
@@ -1,3 +1,13 @@
+const chunkArray = (arr, chunkSize) => {
+  const results = [];
+
+  while (arr.length) {
+    results.push(arr.splice(0, chunkSize));
+  }
+
+  return results;
+};
+
 const requestUsers = (gender) => {
   const ajax = new XMLHttpRequest(); // eslint-disable-line no-undef
   const url = `https://randomuser.me/api/?results=9&gender=${gender}&inc=name,email,picture`;
@@ -6,28 +16,30 @@ const requestUsers = (gender) => {
     if (ajax.status === 200) {
       const response = JSON.parse(ajax.response);
 
-      response.results.map((per) => { // eslint-disable-line array-callback-return
-        const newCard = document.createElement('div'); // eslint-disable-line no-undef
-        newCard.classList.add('column', 'is-one-third');
-        newCard.innerHTML = `
-            <div class='card'>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-48x48">
-                                <img src="${per.picture.thumbnail}" alt="User Image">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="title is-4">${per.name.first} ${per.name.last}</p>
-                            <p class="subtitle is-6">${per.email}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        document.getElementById('users').append(newCard); // eslint-disable-line no-undef
-      });
+      console.log(chunkArray(response.results, 3));
+
+    //   response.results.map((per) => { // eslint-disable-line array-callback-return
+    //     const newCard = document.createElement('div'); // eslint-disable-line no-undef
+    //     newCard.classList.add('column', 'is-one-third');
+    //     newCard.innerHTML = `
+    //         <div class='card'>
+    //             <div class="card-content">
+    //                 <div class="media">
+    //                     <div class="media-left">
+    //                         <figure class="image is-48x48">
+    //                             <img src="${per.picture.thumbnail}" alt="User Image">
+    //                         </figure>
+    //                     </div>
+    //                     <div class="media-content">
+    //                         <p class="title is-4">${per.name.first} ${per.name.last}</p>
+    //                         <p class="subtitle is-6">${per.email}</p>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     `;
+    //     document.getElementById('users').append(newCard); // eslint-disable-line no-undef
+    //   });
     } else {
       console.log('The request failed!');
     }
