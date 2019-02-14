057363c7e3aa378217545d9ddb7d575be0df11ce
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-02-13 23:49:50 -0800

    Use fetch for http request

--- assets/js/generate-users.js
+++ assets/js/generate-users.js
@@ -1,3 +1,50 @@
+const processUserData = (data) => {
+  // Throw error if error returned from randomuser.me
+  if (data.hasOwnProperty('error')) { // eslint-disable-line
+    throw `randomuser.me error: ${data.error}`;
+  }
+
+  // Chunk the returned users into groups of 3
+  let chunked = chunkArray(data.results, 3); // eslint-disable-line
+
+  let usersContent = '';
+
+  // Prepare each user chunk onto it's own columned row
+  chunked.forEach((chunk) => {
+    let row = '<div class="columns">';
+    chunk.forEach((person) => {
+      const firstName = capFirstLetter(person.name.first); // eslint-disable-line
+      const lastName = capFirstLetter(person.name.last); // eslint-disable-line
+
+      // Build out the user card per user
+      row += `
+        <div class="column is-one-third">
+            <div class='card'>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="${person.picture.thumbnail}" alt="User Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">${firstName} ${lastName}</p>
+                            <p class="subtitle is-6">${person.email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `;
+    });
+    row += '</div>';
+    usersContent += row;
+  });
+
+  // Display the generated user list to the end user
+  document.getElementById('users').innerHTML = usersContent; // eslint-disable-line
+};
+
 const requestUsers = (event) => {
   const gender = event.target.id;
   // Check if gender is properly set
@@ -7,64 +54,20 @@ const requestUsers = (event) => {
     throw `Incorrect gender passed: ${gender}`;
   }
 
-  const ajax = new XMLHttpRequest(); // eslint-disable-line
-  // Prepare ajax url
-  const url = `https://randomuser.me/api/?results=9&gender=${gender}&inc=name,email,picture`;
-
-  ajax.onload = () => {
-    if (ajax.status === 200) {
-      const response = JSON.parse(ajax.response);
-      // Throw error if error returned from randomuser.me
-      if (response.hasOwnProperty('error')) { // eslint-disable-line
-        throw `randomuser.me error: ${response.error}`;
+  fetch(`https://randomuser.me/api/?results=9&gender=${gender}&inc=name,email,picture`) // eslint-disable-line
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
       }
-
-      // Chunk the returned users into groups of 3
-      let chunked = chunkArray(response.results, 3); // eslint-disable-line
-
-      let usersContent = '';
-
-      // Prepare each user chunk onto it's own columned row
-      chunked.forEach((chunk) => {
-        let row = '<div class="columns">';
-        chunk.forEach((person) => {
-          const firstName = capFirstLetter(person.name.first); // eslint-disable-line
-          const lastName = capFirstLetter(person.name.last); // eslint-disable-line
-
-          // Build out the user card per user
-          row += `
-            <div class="column is-one-third">
-                <div class='card'>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-left">
-                                <figure class="image is-48x48">
-                                    <img src="${person.picture.thumbnail}" alt="User Image">
-                                </figure>
-                            </div>
-                            <div class="media-content">
-                                <p class="title is-4">${firstName} ${lastName}</p>
-                                <p class="subtitle is-6">${person.email}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          `;
-        });
-        row += '</div>';
-        usersContent += row;
-      });
-
-      // Display the generated user list to the end user
-      document.getElementById('users').innerHTML = usersContent; // eslint-disable-line
-    } else {
-      throw `The request failed! ${ajax.status} status returned`;
-    }
-  };
-
-  ajax.open('GET', url);
-  ajax.send();
+      throw `Fetch to url "${response.url}" failed with status: ${response.status}`;
+    })
+    .then((data) => {
+      processUserData(data);
+    })
+    .catch((error) => {
+      document.getElementById('users').innerHTML = 'Whoops! Something went wrong, check console for more details'; // eslint-disable-line
+      console.log(error);
+    });
 };
 
 // Add Event Listeners
